36c5ef73f9e45d56f40749f1af113487ba381279
Hunter Trujillo <cryptoquick@pm.me>
2019-02-24 02:28:04 -0700

    Linter.

--- hashd/hashd.py
+++ hashd/hashd.py
@@ -3,6 +3,7 @@ import argparse
 
 from commands import init, add_work, hash_bench
 
+
 def parse(args_str: List[str]) -> None:
     parser = argparse.ArgumentParser(prog="hashd")
     parser.set_defaults(func=lambda a: parser.print_help())
@@ -16,15 +17,13 @@ def parse(args_str: List[str]) -> None:
     init_subparser.set_defaults(func=init.init)
 
     add_work_subparser = subparsers.add_parser(
-        "add-work",
-        help="Adds PoW to a block signature",
+        "add-work", help="Adds PoW to a block signature"
     )
     add_work_subparser.add_argument("signature")
     add_work_subparser.set_defaults(func=add_work.add_work)
 
     hash_bench_subparser = subparsers.add_parser(
-        "hash-bench",
-        help="Benchmark to show lowest hash after 1, 10, and 100 seconds",
+        "hash-bench", help="Benchmark to show lowest hash after 1, 10, and 100 seconds"
     )
     hash_bench_subparser.set_defaults(func=hash_bench.hash_bench)
 
