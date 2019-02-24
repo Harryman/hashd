184cb8c3001c3ce506e83cf67f9dd147fd60a85a
Hunter Trujillo <cryptoquick@pm.me>
2019-02-24 02:24:27 -0700

    Add add-work and hash-bench commands.

--- hashd/hashd.py
+++ hashd/hashd.py
@@ -1,8 +1,7 @@
 from typing import List
 import argparse
 
-from commands.init import init
-
+from commands import init, add_work, hash_bench
 
 def parse(args_str: List[str]) -> None:
     parser = argparse.ArgumentParser(prog="hashd")
@@ -14,7 +13,20 @@ def parse(args_str: List[str]) -> None:
         "init",
         help="Generates and stores a local private key and provides the mnemonic",
     )
-    init_subparser.set_defaults(func=init)
+    init_subparser.set_defaults(func=init.init)
+
+    add_work_subparser = subparsers.add_parser(
+        "add-work",
+        help="Adds PoW to a block signature",
+    )
+    add_work_subparser.add_argument("signature")
+    add_work_subparser.set_defaults(func=add_work.add_work)
+
+    hash_bench_subparser = subparsers.add_parser(
+        "hash-bench",
+        help="Benchmark to show lowest hash after 1, 10, and 100 seconds",
+    )
+    hash_bench_subparser.set_defaults(func=hash_bench.hash_bench)
 
     args = parser.parse_args(args_str)
     args.func(args)
